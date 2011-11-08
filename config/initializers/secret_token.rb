9ca42a30d83b8b2a1b5c3cd24a89101133683d08
menslow <michael.enslow@gmail.com>
2011-11-07 23:23:43 -0500

    added user model and controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Summnr::Application.config.secret_token = '35deba88593896907aa9387893287153965408364facbc51dfcaca703f0b314687ece975cc1ac9c32ef31633e7de2cb10813b1d0593615134d4c7e10818f5f19'
